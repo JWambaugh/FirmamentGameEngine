0a4b43456d20b3a20f505a97afc306fd66f4f44d
Jordan Wambaugh <jordan@wambaugh.org>
2012-02-16 22:50:42 -0800

    added FRenderer.hx

--- src/com/firmamentengine/firmament/FRenderable.hx
+++ src/com/firmamentengine/firmament/FRenderable.hx
@@ -1,5 +1,5 @@
 package com.firmamentengine.firmament;
-
+import com.firmamentengine.firmament.FRenderer;
 /**
  * ...
  * @author Jordan Wambaugh
@@ -7,10 +7,24 @@ package com.firmamentengine.firmament;
 
 class FRenderable extends FWorldPositional
 {
-
+	var renderer:FRenderer;
+	var imageScale:Float;
+	var zPosition:Float;
 	public function new() 
 	{
 		super();
+		this.imageScale = 100;
+		this.zPosition = 0;
+	}
+	
+	
+	
+	public function getRenderer():FRenderer {
+		return this.renderer;
+	}
+	
+	public function setRenderer(r:FRenderer) {
+		this.renderer = r;
 	}
 	
 }
---
+++ src/com/firmamentengine/firmament/FRenderer.hx
@@ -0,0 +1,14 @@
+package com.firmamentengine.firmament;
+
+/**
+ * ...
+ * @author Jordan Wambaugh
+ */
+
+interface FRenderer 
+{
+
+	function render(cxt:Dynamic,item:Dynamic,camera:Dynamic):Void;
+	
+	
+}
