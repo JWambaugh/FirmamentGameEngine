17b510fe3872e23837c97fe8962100bb5c25d787
Jordan Wambaugh <jordan@wambaugh.org>
2012-05-03 18:07:30 -0700

    Build new haxelib package

---
+++ buildLib.sh
@@ -0,0 +1,12 @@
+rm -rf haxelib
+mkdir haxelib
+mkdir haxelib/firmament
+cp haxelib.xml haxelib/firmament
+cp -r src/firmament haxelib/firmament
+haxe -xml haxelib/firmament/haxedoc.xml.tmp bin/flash/haxe/debug.hxml
+#use xmlstarlet to filter the xml document file to only show firmament items
+./xml ed -d "//class[contains(@path,'firmament')=false]" -d "//typedef[contains(@path,'firmament')=false]" -d "//enum[contains(@path,'firmament')=false]" haxelib/firmament/haxedoc.xml.tmp > haxelib/firmament/haxedoc.xml
+rm  haxelib/firmament/haxedoc.xml.tmp 
+cd haxelib
+zip -r firmament.zip firmament
+cd ..
