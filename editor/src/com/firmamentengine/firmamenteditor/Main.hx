07d8ff29d583185343f5f365e510e9273b468a49
Jordan Wambaugh <jordan@wambaugh.org>
2012-05-04 19:40:07 -0700

    More progress on ui library

--- editor/src/com/firmamentengine/firmamenteditor/Main.hx
+++ editor/src/com/firmamentengine/firmamenteditor/Main.hx
@@ -7,6 +7,7 @@ import nme.Lib;
 
 import nme.events.Event;
 import nme.events.MouseEvent;
+import firmament.ui.FDialog;
 
 /**
  * ...
@@ -20,6 +21,12 @@ class Main
 
    public static function main()
    {
+	    #if (flash9 || flash10)
+		haxe.Log.trace = function(v,?pos) { untyped __global__["trace"](pos.className+"#"+pos.methodName+"("+pos.lineNumber+"):",v); }
+		#elseif flash
+		haxe.Log.trace = function(v,?pos) { flash.Lib.trace(pos.className+"#"+pos.methodName+"("+pos.lineNumber+"): "+v); }
+		#end
+	   
 		var stage = Lib.current.stage;
 		stage.align = StageAlign.TOP_LEFT;
 		stage.scaleMode = StageScaleMode.NO_SCALE;
@@ -27,5 +34,7 @@ class Main
 	    var project = new Project();
 		var projectEditor = new ProjectSettings(project);
 		stage.addChild(projectEditor);
+		FDialog.alert("Welcome to the Firmament Editor!\nThis is still a work in progress, so please bear with us!","Welcome to Firmament");
+		
    }
 }
--- editor/src/com/firmamentengine/firmamenteditor/ui/ProjectSettings.hx
+++ editor/src/com/firmamentengine/firmamenteditor/ui/ProjectSettings.hx
@@ -1,10 +1,13 @@
 package com.firmamentengine.firmamenteditor.ui;
 import com.firmamentengine.firmamenteditor.Project;
 import firmament.ui.FButton;
+import firmament.ui.layout.FHBox;
+import firmament.ui.layout.FVBox;
 import nme.display.Sprite;
 import firmament.ui.FTextLabel;
 import firmament.ui.FLineEdit;
 import firmament.ui.FWindow;
+import nme.events.MouseEvent;
 
 /**
  * ...
@@ -27,33 +30,24 @@ class ProjectSettings extends FWindow
 		this.setTitle("Project Settings");
 		this.project = project;
 		
-		canvas.addChild(new FTextLabel("Project File:"));
-		
-		projectFileTxt = new FLineEdit("", 75);
-		canvas.addChild(projectFileTxt);
-		
-		canvas.addChild(new FTextLabel("Base Dir:",0,30));
-		
-		baseDirTxt = new FLineEdit("", 75,30);
-		canvas.addChild(baseDirTxt);
-		
-		canvas.addChild(new FTextLabel("Entity Dir:",0,60));
-		entityDirTxt = new FLineEdit("",75,60);
-		canvas.addChild(entityDirTxt);
-		
-		canvas.addChild(new FTextLabel("Map Dir:",0,90));
-		mapDirTxt = new FLineEdit("",75,90);
-		canvas.addChild(mapDirTxt);
-		
-		
-		var saveButton = new FButton("Load Settings",0,120);
-		canvas.addChild(saveButton);
-		
-		var loadButton = new FButton("Save Settings",100,120);
-		canvas.addChild(loadButton);
+
+		canvas.addChild(new FVBox([
+			new FHBox([new FTextLabel("Project File:"), projectFileTxt = new FLineEdit()])
+			,new FHBox([new FTextLabel("Base Dir:",0,30), baseDirTxt = new FLineEdit("", 75,30)])
+			,new FHBox([new FTextLabel("Entity Dir:",0,60), entityDirTxt = new FLineEdit("",75,60)])
+			,new FHBox([new FTextLabel("Map Dir:",0,90), mapDirTxt = new FLineEdit("",75,90)])
+			,new FHBox([new FButton("Load Settings",0,120,save),new FButton("Save Settings",100,120,load)])
+	]));
 		
 		
 		this.setCanvas(canvas);
 	}
+	
+	function load(e:MouseEvent) {
+	}
+	
+	function save(e:MouseEvent) {
+	}
+	
 	
 }
