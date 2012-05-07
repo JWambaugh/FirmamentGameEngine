0c7075ab202be1f69d57a8bdfe15ea62e1256359
Jordan Wambaugh <jordan@wambaugh.org>
2012-05-06 19:17:37 -0700

    Reorganized structure, added custom box2d modified to run in neko.

--- buildLib.sh
+++ buildLib.sh
@@ -2,7 +2,7 @@ rm -rf haxelib
 mkdir haxelib
 mkdir haxelib/firmament
 cp haxelib.xml haxelib/firmament
-cp -r src/firmament haxelib/firmament
+cp -r lib/* haxelib/firmament
 haxe -xml haxelib/firmament/haxedoc.xml.tmp bin/flash/haxe/debug.hxml
 #use xmlstarlet to filter the xml document file to only show firmament items
 ./xml ed -d "//class[contains(@path,'firmament')=false]" -d "//typedef[contains(@path,'firmament')=false]" -d "//enum[contains(@path,'firmament')=false]" haxelib/firmament/haxedoc.xml.tmp > haxelib/firmament/haxedoc.xml
