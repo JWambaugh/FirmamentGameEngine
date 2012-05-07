933bbc9cce0c5197a3e015338e087148434ae6ab
Jordan Wambaugh <jordan@wambaugh.org>
2012-05-07 16:49:25 -0700

    new haxelib build

--- buildLib.sh
+++ buildLib.sh
@@ -1,12 +1,14 @@
 rm -rf haxelib
 mkdir haxelib
 mkdir haxelib/firmament
-cp haxelib.xml haxelib/firmament
 cp -r lib/* haxelib/firmament
-haxe -xml haxelib/firmament/haxedoc.xml.tmp bin/flash/haxe/debug.hxml
+cd tools
+haxe -main Cli -neko run.n -cp ../lib -lib hxJson2 -lib nme
+cd ..
+cp tools/run.n haxelib/firmament
 #use xmlstarlet to filter the xml document file to only show firmament items
-./xml ed -d "//class[contains(@path,'firmament')=false]" -d "//typedef[contains(@path,'firmament')=false]" -d "//enum[contains(@path,'firmament')=false]" haxelib/firmament/haxedoc.xml.tmp > haxelib/firmament/haxedoc.xml
-rm  haxelib/firmament/haxedoc.xml.tmp 
+#./xml ed -d "//class[contains(@path,'firmament')=false]" -d "//typedef[contains(@path,'firmament')=false]" -d "//enum[contains(@path,'firmament')=false]" haxelib/firmament/haxedoc.xml.tmp > haxelib/firmament/haxedoc.xml
+#rm  haxelib/firmament/haxedoc.xml.tmp 
 cd haxelib
 zip -r firmament.zip firmament
 cd ..
