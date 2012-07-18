992cc54702e7230f47767336c91fd657a5add777
Jordan Wambaugh <jordan@wambaugh.org>
2012-07-17 19:42:29 -0700

    Fixed compilation bug in FStyleHelper.hx

--- lib/firmament/ui/FStyleHelper.hx
+++ lib/firmament/ui/FStyleHelper.hx
@@ -5,10 +5,12 @@ package firmament.ui;
  * use as a mixin for extra tools with config values
  */
 class FStyleHelper {
-	public static function get(o:Dynamic, key:String, ?def:Dynamic=null){
-		var val = Reflect.field(o,key);
-		if(Std.is(val,null)) val = def;
-		return val;
+	public static function get(o:Dynamic, key:String, ?def:Dynamic=null):Dynamic{
+		if(Reflect.hasField(o,key))	
+			return Reflect.field(o,key);
+		
+		return def;
+		
 	}
 
 }
