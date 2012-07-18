5c8131864c8a44ec01d2e63ce1a390ee76e94a2f
Jordan Wambaugh <jordan@wambaugh.org>
2012-07-17 20:17:54 -0700

    New haxeilb release.
    Completed windows haxelib build script

--- buildLib.bat
+++ buildLib.bat
@@ -1,8 +1,13 @@
+set path="C:\Program Files\WinRAR\";%path%
+del haxelib\*.zip
+del haxelib /f
 mkdir haxelib
 mkdir haxelib\firmament
-cp -r lib\* haxelib\firmament
+xcopy  lib\*.* haxelib\firmament /e /y
 haxe -xml haxelib\firmament\haxedoc.xml bin\flash\haxe\debug.hxml
-zip -r firmament.zip haxelib\firmament
+cd haxelib
+winrar.exe a -afzip firmament.zip firmament
+cd ..
 cd tools
 haxe -main Cli -neko run.n -cp ../lib -lib hxJson2 -lib nme
 cd..
