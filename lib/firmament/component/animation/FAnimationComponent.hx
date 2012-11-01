f2ddb1f6f86b3a819e8e99adcc8f7768526e1aaa
Jordan Wambaugh <jordan@wambaugh.org>
2012-11-01 11:13:17 -0700

    began work on animation system

---
+++ lib/firmament/component/animation/FAnimationComponent.hx
@@ -0,0 +1,27 @@
+package firmament.component.animation;
+
+import frimament.core.FAnimation;
+import firmament.component.animation.FAnimationComponentInterface;
+import firmament.component.base.FEntityComponent;
+
+class FAnimationComponent extends FEntityComponent, implements FAnimationComponentInterface{
+	
+	var _currentAnimation:FAnimation;
+	var _currentFrame:Int;
+	var _timeSinceLastFrameChange:Float;
+
+	public function new(){
+		super();
+		_currentFrame = 0;
+		_timeSinceLastFrameChange=0;
+	}
+
+	override public function init(config:Dynamic){
+
+	}
+
+	override public function getType(){
+		return "animation";
+	}	
+
+}
---
+++ lib/firmament/core/FAnimation.hx
@@ -0,0 +1,75 @@
+package firmament.core;
+
+import firmament.core.FTilesheet;
+import firmament.core.FTilesheetManager;
+
+class FAnimation {
+
+	private var _tilesheet:FTilesheet;
+	private var _frames:Array<Int>;
+	private var _timeBetweenFrames:Float;
+
+	private var _config:Dynamic;
+	
+	public function new(){
+
+	}
+
+	public function init(config:Dynamic){
+		if(Std.is(config.tilesheetFile,String)){
+			_tilesheet = FTilesheetManager.getInstance().getTilesheetFromDifinitionFile(config.tilesheetFile);
+		}
+		else if(Std.is(config.tilesheet,FTilesheet)){
+			_tilesheet = config.tilesheet;
+		} else {
+			throw "Tilesheet not provided";
+		}
+
+		if(Std.is(config.frames,Array)){
+			for(el in config.frames){
+				_frames.push(el);
+			}
+		}else{
+			throw "Frames not provided";
+		}
+
+		if(Std.is(config.timeBeweenFrames,Float)){
+			_timeBetweenFrames = config.timeBeweenFrames;
+		}else {
+			_timeBetweenFrames = 0.03;
+		}
+	}
+
+	public function getTilesheet():FTilesheet{
+		returh _tilesheet;
+	}
+
+	public function setTilesheet(t:FTilesheet){
+		_tilesheet = t;
+	}
+
+	public function getTimeBetweenFrames(){
+		return _timeBetweenFrames;
+	}
+
+	/**
+	 * Function: getTileIndexForFrame
+	 * returns the tilesheet tile index for the specified animation frame
+	 */
+	public function getTileIndexForFrame(frame:Int):Int{
+		if(frame >=_frames.length) throw 'Frame of '+frame+' doesn\'t exist';
+		return _frames[frame];
+	}
+
+	/**
+	 * Function: getNextFrame
+	 * Given the provided fram index, it returns the next one in the animation sequence.
+	 */
+	public function getNextFrame(currentFrame:Int):Int{
+		return (currentFrame+1)%_frames.length;
+	}
+
+
+	
+
+}
