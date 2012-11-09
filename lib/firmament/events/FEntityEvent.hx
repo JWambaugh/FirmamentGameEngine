47cb8a90dfd25df1ae24333412903204b44f529e
Jordan Wambaugh <jordan@wambaugh.org>
2012-11-09 10:35:07 -0800

    added now event type

---
+++ lib/firmament/events/FEntityEvent.hx
@@ -0,0 +1,19 @@
+
+package firmament.events;
+
+import nme.events.Event;
+import firmament.core.FEntity;
+class FEntityEvent extends Event {
+
+	var _entity:FEntity;
+
+	public function new(event:String,entity:FEntity){
+		super(event);
+		_entity = entity;
+	}
+
+	public function getEntity():FEntity{
+		return _entity;
+	}
+
+}
