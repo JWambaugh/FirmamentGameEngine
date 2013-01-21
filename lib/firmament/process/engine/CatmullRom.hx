8d37791c894ce57c963c1bc18816d461912b3454
Robert Wlaschin <raw77_m@yahoo.com>
2013-01-20 22:44:04 -0800

    Updated spline class
    Added rendering of curve

--- lib/firmament/process/engine/CatmullRom.hx
+++ lib/firmament/process/engine/CatmullRom.hx
@@ -2,75 +2,93 @@
 package firmament.process.engine;
 
 import nme.geom.Point;
-import firmament.core.FCamera;
+import firmament.core.FVector;
 
 class CatmullRom
 {
-    private var ps:Array<Dynamic>; // = [];
+    private var ps:Array<FVector>; // = [];
     private var step:Int; // = [];
     private var position:Float;
+    private var p0:FVector;
+    private var p1:FVector;
+    private var p2:FVector;
+    private var p3:FVector;
+    private var rate:Float;
+    private var startOffset:FVector;
+    private var endOffset:FVector;
 
     /*CatmullRom*/
-    public function new ()
+    public function new (points:Array<FVector>,totalTime:Float)
     {
     	ps = [];
 
         step = -1;
         position = 1;
 
-        var N:Int = Std.int(3 + 1/*+ 10 * Math.random ()*/);
-        /*for ( i in 0 ... N ) {
-            ps.push (new Point (200-400 * Math.random (), 200-400 * Math.random ()));
-        }*/
+        var N:Int = Std.int(3 + 10 * Math.random ());
+        for ( i in 0 ... N ) {
+            ps.push (new FVector (200-400 * Math.random (), 200-400 * Math.random ()));
+        }
+        /*ps.push (new FVector (-200, -200));
+        ps.push (new FVector (0,0));
+        ps.push (new FVector (200, 200));*/
+        rate = ps.length / 10.0;
 
-        ps.push (new Point (-200, -200));
-        ps.push (new Point (0,0));
-        ps.push (new Point (200, 200));
+        if(Std.is(points,Array)) {
+            ps = [];
+            for(point in points) {
+                ps.push(point);
+            }
+        }
+
+        if(Math.isNaN(totalTime) == false) {
+            rate = ps.length / totalTime;
+        }
     }
 
-    public function loop (delta:Float):Point 
+    public function setStartTarget() {
+
+    }
+
+    public function setEndTarget() {
+
+    }
+
+    public function reset() {
+        step = -1;
+        position = 1;
+    }
+
+    public function progress() {
+        return step/(ps.length-1);
+    }
+
+    public function loop (delta:Float):FVector 
     {
-        position=position+delta;
+        var updated:Bool = false;
+        position=position+(delta*rate);
         while(position > 1) {
-            trace("Step: " + step);
+            updated=true;
             step = (step+1) % (ps.length-1);
-            trace("Step: " + step);
             position=position-1;
         }
 
-        trace("Step: " + step);
-        trace("Position: " + position);
-
-         // draw spline
-        /*for (i in 0 ... ps.length)*/ {
+        if(updated) {
             var s0 = ((step-1)>=0)?step-1:0; var s1 = step;
             var s2=((step+1)<ps.length)?step+1:step; 
             var s3=((step+2)<ps.length)?step+2:step;
-            var p0:Point = ps [s0];
-            var p1:Point = ps [s1];
-            var p2:Point = ps [s2];
-            var p3:Point = ps [s3];
-
-            //s.graphics.moveTo (p1.x, p1.y);
-            /*for ( j  in 1...101 ) { */
-                var q:Point = spline (p0, p1, p2, p3, position);
-                trace("Points - " + q.x + "," + q.y);
-                //s.graphics.lineTo (q.x, q.y);
-                // want to draw the tangent eventually
-            /*}*/
-
-            return q;
-
-            /*if (counter++ > 0) {
-                        camera.graphics.lineTo((pos.x+vec.x-cameraPos.x)*camera.getZoom(), (pos.y+vec.y-cameraPos.y)*camera.getZoom());
-                    }
-                    pvec = vec;
-                    camera.graphics.moveTo(( pos.x+pvec.x - cameraPos.x)*camera.getZoom(), (pos.y+pvec.y - cameraPos.y)*camera.getZoom());
-            */
+            p0 = ps [s0];
+            p1 = ps [s1];
+            p2 = ps [s2];
+            p3 = ps [s3];
         }
+
+        var q:FVector = spline (p0, p1, p2, p3, position);
+        // trace("Points - " + q.x + "," + q.y);
+        return q;
     }
 
-    public function getPoints():Array<Dynamic> {
+    public function getPoints():Array<FVector> {
         return ps;
     }
 
@@ -78,9 +96,9 @@ class CatmullRom
     * Calculates 2D cubic Catmull-Rom spline.
     * @see http://www.mvps.org/directx/articles/catmull/
     */ 
-    public function spline (p0:Point, p1:Point, p2:Point, p3:Point, t:Float):Point 
+    public function spline (p0:FVector, p1:FVector, p2:FVector, p3:FVector, t:Float):FVector 
     {
-        return new Point (
+        return new FVector (
             0.5 * ((          2*p1.x) +
                 t * (( -p0.x           +p2.x) +
                 t * ((2*p0.x -5*p1.x +4*p2.x -p3.x) +
--- lib/firmament/process/engine/FSplineTweener.hx
+++ lib/firmament/process/engine/FSplineTweener.hx
@@ -6,6 +6,7 @@ import firmament.core.FCamera;
 import firmament.process.base.FProcess;
 import firmament.process.engine.FLinearTweener;
 import firmament.process.engine.CatmullRom;
+import firmament.core.FVector;
 
 import nme.geom.Point;
 import nme.display.Graphics;
@@ -13,19 +14,32 @@ import nme.display.Graphics;
 class FSplineTweener extends FProcess {
 
     private var spline:CatmullRom;
-    private var q:Point;
+    private var q:FVector;
+    private var pts:Array<FVector>;
     private var graphics:Graphics;
 
 	public function new(parameters:Dynamic) {
-		super();		
+		super();
 		FGame.getInstance().addProcess("splineTweener",this);
-		spline = new CatmullRom();
-		/*if(parameters.time != null) {
-			_duration = parameters.time+0.0;
-		}*/
+		spline = new CatmullRom(null,Math.NaN);
 		if( parameters.graphicsObject != null ) {
 			graphics = parameters.graphicsObject;
 		}
+
+		//build spline path
+		pts = [];
+		var delta:Float = 1.0/20;
+		var progress:Float = spline.progress();
+		while( progress < 1 ) {
+			pts.push(spline.loop(delta));
+			var nextProgress:Float = spline.progress();
+			if(nextProgress != progress) {
+				if (nextProgress < progress) {progress = 1;} 
+				else {progress = nextProgress;}
+				trace("Progress - " + progress);
+			}
+		}
+		reset();
 	}
 
 	/*override*/ public function pause() { 
@@ -44,42 +58,23 @@ class FSplineTweener extends FProcess {
 		// Ti = 0.5 * ( Pi+1 - Pi-1 ) // Catmull-Rom splines
 
 		var delta = this._manager.getFrameDelta();
-		trace("Delta: " + delta);
+		// trace("Delta: " + delta);
 		q=spline.loop(delta);
-
-		/*var ts:Float = t / steps;
-        
-        var pt:B2Vec2 = 0.5 * ( 
-        				(2*p1) +  ts * ( 
-        					(-p0+p2) + ts * (
-        						  (2*p0 -5*p1 +4*p2 -p3) + ts * (-p0.x +3*p1.x -3*p2.x +p3.x)
-        						)
-        					)
-        				)
-        */
-
-		/*
-		var s:Float = t / steps
-		var s3:Float = s*s*s;
-		var s2:Float = s*s;
-		var h1:Float =  2*s3 - 3*s2 + 1;
-		var h2:Float = -2*s3 + 3*s2;
-		var h3:Float =    s3 - 2*s2 + s;
-		var h4:Float =    s3 -   s2;*/
-
-		// Ti = 0.5 * ( Pi-1 - Pi+1 )
-		// B2Vec2 p = h1*P1 + h2*P2 + h3*T1 + h4*T2;
-		// set object position to p
 	}
 
-	public function renderPoint() {		
-        // graphics.drawCircle((q.x-cameraPos.x)*camera.getZoom(),(q.y-cameraPos.y)*camera.getZoom(),100);
-        
-        var points:Array<Dynamic> = spline.getPoints();
+	public function renderPoint() {        
+        var points:Array<FVector> = spline.getPoints();
 
         graphics.lineStyle(2,0xcccc0);
-        graphics.beginFill(0xcc0000);
 
+		/*graphics.moveTo(pts[0].x+300,pts[0].y+400);
+        for ( index in 1...(pts.length-1) ) {
+        	var point:FVector = pts[index];
+        	graphics.lineStyle(2,0xcccc0);
+        	graphics.lineTo(point.x+300,point.y+400);
+        }*/
+
+        graphics.beginFill(0xcc0000);
         for( point in points ) {
         	graphics.drawCircle(point.x+300,point.y+400,5);
         }
@@ -87,5 +82,7 @@ class FSplineTweener extends FProcess {
 		graphics.lineStyle(1,0xFFFF00);
         graphics.beginFill(0xFF0000);
         graphics.drawCircle(q.x+300,q.y+400,2);
+        graphics.lineStyle(0,0,0);
+        graphics.endFill();
 	}
 }
