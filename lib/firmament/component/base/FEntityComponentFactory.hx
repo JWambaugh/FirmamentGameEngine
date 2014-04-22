3b74ea5715c7eaa61586f10e943ad7a488ee97bc
Gurney <hidukeleto@yahoo.com>
2014-04-21 22:24:53 -0700

    Added ComponentFactoryComponent.hx
    debugged

--- lib/firmament/component/base/FEntityComponentFactory.hx
+++ lib/firmament/component/base/FEntityComponentFactory.hx
@@ -14,6 +14,7 @@ import firmament.component.render.FWireframeRenderComponent;
 import firmament.component.render.FLineRenderComponent;
 import firmament.component.render.FTextRenderComponent;
 import firmament.component.spline.FSimpleFollowSplineComponent;
+import firmament.component.system.FComponentFactoryComponent;
 
 import firmament.component.event.FCollisionEventMapperComponent;
 import firmament.component.sound.FSoundComponent;
@@ -57,6 +58,7 @@ class FEntityComponentFactory{
 			,"entityContainer":"firmament.component.ui.FEntityContainerComponent"
 			,"followSpline":"firmament.component.spline.FSimpleFollowSplineComponent"
 			,"text":"firmament.component.render.FTextRenderComponent"
+			,"componentFactory":"firmament.component.system.FComponentFactoryComponent"
 		};
 
 		var cls = Reflect.field(map,type);
--- lib/firmament/component/system/FComponentFactoryComponent.hx
+++ lib/firmament/component/system/FComponentFactoryComponent.hx
@@ -2,29 +2,41 @@
 package firmament.component.system;
 import firmament.component.base.FEntityComponent;
 import firmament.core.FEvent;
+import firmament.component.base.FEntityComponentFactory;
 
 /*
 	Class: FComponentFactoryComponent
 	Adds a component on event fire
 */
+
 class FComponentFactoryComponent extends FEntityComponent{
 
-	var _eventFired: Dynamic;
+	var _event: Dynamic;
 
 	public function new(){
 		super();
 	}
 
-	override public function init(){
-		if (Reflect.isObject(config.event)){
-			_eventFired = event;
+	override public function init(config:Dynamic){
+		if (Reflect.isObject(config.events)){
+			_event = config.events;
 		}else{
 			throw "No event fired to trigger new component";
 		}
-	
+
+				for(eventName in Reflect.fields(_event)){
+					on(_entity,eventName,function(e:FEvent){
+						trace ("woot im here!");
+						var cConfig = Reflect.field(_event, eventName);
+						var component = FEntityComponentFactory.createComponent(cConfig.componentName,"FComponentFactoryComponent");
+						component.setConfig(cConfig);
+						_entity.setComponent(component);
+						component.init(cConfig);
+					});
+			}
 	}
 
 	override public function getType(){
-		return "factoryComponent";
+		return "ComponentFactoryComponent";
 	}
 }
