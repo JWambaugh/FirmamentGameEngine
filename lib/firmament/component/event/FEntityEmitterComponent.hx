13af515b84c5cdc0fce4204cb0d2e90c3c564325
Robert Wlaschin <raw77_m@yahoo.com>
2015-02-09 11:35:18 -0800

    Push creation to onStep

--- lib/firmament/component/event/FEntityEmitterComponent.hx
+++ lib/firmament/component/event/FEntityEmitterComponent.hx
@@ -22,40 +22,45 @@ class FEntityEmitterComponent extends FEntityComponent{
 
 	override public function init(config:FConfig){
 
-		on(_entity,config.get('listen',String),this,function(e:FEvent){
-            
-            var amount:Int = config.get('amount', Int, 1);
-            FLog.debug("amount to spawn: "+amount);
-            for(n in 0...amount){
-				var ent = FEntityFactory.createEntity(
-						config.getNotNull('entity',Dynamic));
-				var angle:Float;
-				if(config.get("angleOffset",String)=="random"){
-					angle = Math.random()*6.28318530718;
-				}else{
-					angle=config.get("angleOffset",Float);
-				}
-				
-				var originEntity = _entity;
-				
-				//if event received is an entity event, use that entity as the origin instead
-				if(Std.is(e,FEntityEvent)){
-					originEntity = cast(e,FEntityEvent).getEntity();
-				}
+		on(_entity,config.get('listen',String),this,
+			function(e:FEvent) {
+				_entity.getGameInstance().doAfterStep(
+					function() {
+			            var amount:Int = config.get('amount', Int, 1);
+			            FLog.debug("amount to spawn: "+amount);
+			            for(n in 0...amount){
+							var ent = FEntityFactory.createEntity(
+									config.getNotNull('entity',Dynamic));
+							var angle:Float;
+							if(config.get("angleOffset",String)=="random"){
+								angle = Math.random()*6.28318530718;
+							}else{
+								angle=config.get("angleOffset",Float);
+							}
+							
+							var originEntity = _entity;
+							
+							//if event received is an entity event, use that entity as the origin instead
+							if(Std.is(e,FEntityEvent)){
+								originEntity = cast(e,FEntityEvent).getEntity();
+							}
 
-				FEntityUtils.emitEntity(originEntity
-					,ent
-					,config.get("speed",Float,0)
-					,angle
-					,config.get("distanceOffset",Float,0.01)
-					,config.get("ignoreParent",Bool,false) );
-				ent.setActive(true);
-            }
-            var trigger = config.get('trigger',String);
-            if(trigger != null){
-                _entity.trigger(new FEvent(trigger));
-            }
-		});
+							FEntityUtils.emitEntity(originEntity
+								,ent
+								,config.get("speed",Float,0)
+								,angle
+								,config.get("distanceOffset",Float,0.01)
+								,config.get("ignoreParent",Bool,false) );
+							ent.setActive(true);
+			            }
+			            var trigger = config.get('trigger',String);
+			            if(trigger != null){
+			                _entity.trigger(new FEvent(trigger));
+			            }
+					}
+				);
+			}
+		);
 		
 	}
 		
