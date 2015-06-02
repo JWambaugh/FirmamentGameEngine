fb66daba963605983fa25c05141cb3a67d41ef92
Robert Wlaschin <raw77_m@yahoo.com>
2015-06-01 19:11:58 -0700

    Addresses several tiling issues
    . Max Offset to address artifacting issues when scrolling tiles
    . Zbuffer order to address flip-flopping while textures are being
    rendered
    . Clean up to logging
    . Added reset event

--- lib/firmament/scene/component/FTiledScroller.hx
+++ lib/firmament/scene/component/FTiledScroller.hx
@@ -20,23 +20,36 @@ class FTiledScroller extends FSceneComponent {
   var _bottomRight:FVector;
   var _screenWidthHeight:FVector;
   var _maxEntities:Int;
+  var _zPositionRange:Array<Float>;
+  var _maximumOffset:FConfig;
+  var _progressiveOffset:FVector;
+  var _reset:Bool;
 
   public function new(gameInstance:Dynamic) {
     super(gameInstance);
     _entities = new Array();
     _maxEntities = 10;
+    _zPositionRange = null;
     useStep(true);
+    _reset = false;
+    _progressiveOffset = new FVector(0,0);
   }
 
   override public function init(config:FConfig):Void {
 
     _enableDebug = config.get('debug',Bool,false);
     _maxEntities = config.get('max',Int,_maxEntities);
+    _zPositionRange = config.get('zPositionRange',Array,_zPositionRange);
+    if ( _zPositionRange != null && _zPositionRange.length != 2 ) {
+      trace("zPositionRange is not an array with 2 items <"+Std.string(_zPositionRange)+">");
+      _zPositionRange = null;
+    }
 
     /*
       {
         "entity" : "string" | FConfig,
         "postitionY" : number | property
+        "zPositionRange" : array<float>
         "speed" : number | Property, // meters per second
         "paused" : bool,
         "listeners" : {
@@ -87,6 +100,24 @@ log("Need to create - " + entityCount);
     for ( e in _entities ) {
       e.setActive(true);
     }
+
+    var listeners:FConfig = _config.get('listeners',Dynamic);
+    for( type in listeners.fields() ) {
+      var eventName = listeners.get(type);
+      log( "listener - " + type + " " + eventName );
+      switch(type) {
+        case "reset": 
+          on( _scene, eventName,resetScrollPosition );
+      }
+    }
+
+    _maximumOffset = _config.get("maximumOffset",Dynamic, {});
+log("Max Offset" + Std.string(_maximumOffset));
+
+  }
+
+  private function resetScrollPosition(e:FEvent) {
+    _reset = true;
   }
 
   override public function step(delta:Float){
@@ -94,6 +125,26 @@ log("Need to create - " + entityCount);
     var sample:Bool = (Math.random() <= .1);
     var max:FVector = new FVector(0,0), min:FVector = new FVector(0,0);
     var halfWidth:Float = _widthHeight.x / 2, halfHeight:Float = _widthHeight.y / 2;
+
+    var xoff:Float = 0;
+    var yoff:Float = 0;
+    if( _maximumOffset.hasField('x',Float) ) {
+      xoff = Math.abs( _maximumOffset.get('x',Float) );
+      if( _progressiveOffset.x >= xoff ) {
+        // accounts for animation jitter when resetting back to original position
+        xoff = _progressiveOffset.x - xoff;
+        _reset = true;
+      }
+    }
+    if( _maximumOffset.hasField('y',Float) ) {
+      yoff = Math.abs( _maximumOffset.get('y',Float) );
+      if( _progressiveOffset.y >= yoff ) {
+        // accounts for animation jitter when resetting back to original position
+        yoff = _progressiveOffset.y - yoff;
+        _reset = true;
+      }
+    }
+
     for ( e in _entities ) {
       var center:FVector = e.getProp('position');
       var locmin_x = center.x - halfWidth, locmin_y = center.y - halfHeight;
@@ -107,38 +158,51 @@ log("Need to create - " + entityCount);
     // check to see if I need to add another entity
     var i = max.x;
     while( i < _bottomRight.x ) {
-      /*if( sample ) {
-        // log( "Adding Entities " + (_bottomRight.x - i) );
-      }*/
       if( _entities.length >= _maxEntities ) { break; }
       _entities.push( getNextEntity() );
       i+=_widthHeight.x;
     }
 
+    var entCount = _entities.length;
+    var zPositionInterval:Null<Float> = null;
+    var zPositionBase:Null<Float> = null;
+    if( _zPositionRange != null ) {
+      zPositionBase = _zPositionRange[0];
+      var posts:Int = entCount - 1;
+      zPositionInterval = (_zPositionRange[1]-_zPositionRange[0]) / (posts > 1?posts:1);
+    }
+
     // figure out the position necessary to fill in the hole
-    var base:Float = Math.min(_topLeft.x,min.x); // choose the smallest
-    while( base < min.x ) {
-      /*if( sample ) {
-      // log( "Finding base position - " + i);
-      }*/
-      base+=_widthHeight.x;
+    var base:Float = 0;
+    if (_reset == true) { 
+      base = _topLeft.x;
+      // update offset to start at an adjusted position 
+      // so there is no popping when entity is reset
+      _progressiveOffset.x = xoff;
+      _progressiveOffset.y = yoff;
+    } else {
+      base = Math.min(_topLeft.x,min.x); // choose the smallest
     }
 
     // shift all of the tiles
-    var speed:Float = _config.get('speed',Float,0) * delta; // this is to correct for rounding errors
+    var speed:Float = _config.get('speed',Float,0) * delta;
+    base += halfWidth - speed;
+    _progressiveOffset.x += Math.abs(speed);
     if(sample) {
       log( "Translating by " + speed );
+      log( "Starting at " + base ); 
     }
-    base += halfWidth - speed;
+    
     for( e in _entities ) {
-      // var pos:Float = e.getProp('positionX');
-      // log( "Shifting entity " + pos + " -> " + base );
-      if( sample ) {
-        log( "Shifting entity to " + base );
-      }
+      if( sample ) {log( "Shifting entity to " + base ); }
       e.setProp('positionX',base);
-      if( e.isActive() == false ) {e.setActive( true ); }
+      if( zPositionBase != null ) {
+        var curZ:Float = e.getProp('positionZ');
+        if( zPositionBase != curZ ) {e.setProp('positionZ', zPositionBase); }
+        zPositionBase += zPositionInterval;
+      }
       base += _widthHeight.x;
+      if( e.isActive() == false ) {e.setActive( true );}
     }
 
     if( sample ) {
@@ -153,6 +217,8 @@ log("Need to create - " + entityCount);
       ent.destruct(); // remove
       ent = null; // destroy reference
     }
+
+    _reset = false;
   }
 
   private function getNextEntity(): FEntity {
@@ -183,19 +249,6 @@ log("Need to create - " + entityCount);
     return ent;
 
   }
-
-  /*override public function getProperties():Array<FPropertyDefinition> {
-    // props limit number of controllers to 1
-    var props:Array<FPropertyDefinition> = [
-          //{
-          //    key:'cameraPosition'
-          //    ,type:FVector
-          //    ,getter:getPosition
-          //    ,setter:setPosition
-          //}
-      ];
-      return props;
-  }*/
 
   override public function destruct(){
     super.destruct();
